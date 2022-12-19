fca31a491a8cf1b8655b7a42408ff92d4d5bcd2e
MuJialuo <50764706+MuJialuo@users.noreply.github.com>
2022-12-19 00:52:33 -0800

    week10_v2

--- run.py
+++ run.py
@@ -3,9 +3,10 @@ app = Flask(__name__)
 
 #dynamic data
 
+
 @app.route("/")
 def template_test():
-    return render_template('template.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
+    return render_template('template.html', my_string="game module here", my_list=[0,1,2,3,4,5])
 
 
 if __name__ == '__main__':
